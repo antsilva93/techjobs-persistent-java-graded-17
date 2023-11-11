2a1df896a46e7b54f0290646cb9a2fee8ccbc6fb
antsilva93 <antsilva93@gmail.com>
2023-11-11 16:33:36 -0600

    task 3 updated Employer and Job models

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,16 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
     @NotBlank(message="Field must not be empty.")
     @Size(max=255,message="Location must be under 256 characters.")
     private String location;
+    @OneToMany
+    @JoinColumn(name="employer_id")
+    private final List<Job> jobs = new ArrayList<>();
 
     public String getLocation() {
         return location;
