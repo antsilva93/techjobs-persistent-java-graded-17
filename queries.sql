19cb94caae3fe75055f1297c605384cc83235b44
antsilva93 <antsilva93@gmail.com>
2023-11-12 10:01:19 -0600

    all tests passing, need to fix app stuff still

--- queries.sql
+++ queries.sql
@@ -14,3 +14,8 @@ WHERE location = "St. Louis City";
 --SQL TASK - Statement to remove job table
 DROP TABLE job;
 --Part 4
+--SQL TASK - query to return names of skills attached to jobs in alphabetical order
+SELECT * FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
