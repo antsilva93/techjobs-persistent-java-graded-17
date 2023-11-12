a976894acb89f7c202cb1d8b57d6fa78049cbeab
antsilva93 <antsilva93@gmail.com>
2023-11-12 09:31:01 -0600

    added annotation to job to match previous push

--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -12,6 +12,7 @@ public class Job extends AbstractEntity {
 
     @ManyToOne
     private Employer employer;
+    @ManyToMany
     private List<Skill> skills = new ArrayList<>();
 
     public Job() {
